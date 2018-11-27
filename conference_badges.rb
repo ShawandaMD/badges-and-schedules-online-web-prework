826c84389e483b5a6a613acfb732cd0b3571f853
Shawanda Davis <shawandadavis93@gmail.com>
2018-11-27 20:22:09 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,7 +9,7 @@ end
 def assign_rooms(attendees)
   attendees.each_with_index.map {|x, index| "Hello, #{x}! You'll be assigned to room #{index += 1}!"}
 end
-
+assign_rooms()
 
 def printer(attendees)
   batch_badge_creator(attendees).each {|x| puts x}
